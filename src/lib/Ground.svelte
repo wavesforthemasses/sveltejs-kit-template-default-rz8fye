81eb063dca79ff0e2fd54f289fab1148613b79db
wavesforthemasses <wavesforthemasses@gmail.com>
2022-05-12 16:53:25 +0200

    1 file modified

--- src/lib/Ground.svelte
+++ src/lib/Ground.svelte
@@ -1,12 +1,15 @@
 <script lang="ts">
+  import * as CANNON from "cannon-es";
   import * as PE from "svelte-cannon";
   import * as THREE from "three";
   import * as SC from "svelte-cubed";
+
+  const data = [...Array(10).keys()].map(i => 0.5 * Math.cos(0.2 * i))
 </script>
 
 <!-- Ground -->
 <PE.Body rotation={[-Math.PI / 2, 0, 0]}>
-  <PE.Plane />
+  <PE.Shape shape={new CANNON.Plane} />
   <PE.Material friction={10} restitution={.9} />
 </PE.Body>
 <SC.Mesh
