25429020d1ae8dff3bc42b86362f5c3e3c8f29cc
wavesforthemasses <wavesforthemasses@gmail.com>
2022-05-14 23:56:42 +0200

    1 file modified

--- src/lib/math.js
+++ src/lib/math.js
@@ -7,7 +7,6 @@ export const vec3hashing = {
 		const deltaY = [...Array(4).keys()].find((i) => y < ((i + 1) * (maxY - minY)) / 4 + minY);
 		const deltaZ = [...Array(4).keys()].find((i) => z < ((i + 1) * (maxZ - minZ)) / 4 + minZ);
 		const delta = parseInt(`${deltaX}${deltaY}${deltaZ}`, 4);
-		console.log(x, y, z);
 		if (p > 1)
 			return vec3hashing.encode({
 				...params,
@@ -21,5 +20,33 @@ export const vec3hashing = {
 				encoded: `${encoded}${vec3hashing.charset[delta]}`
 			});
 		return `${encoded}${vec3hashing.charset[delta]}`;
+	},
+	decode: (params) => {
+		const { encodedString, min = -100, max = 100 } = params;
+		const { minX = min, maxX = max, minY = min, maxY = max, minZ = min, maxZ = max } = params;
+		const [deltaX, deltaY, deltaZ] =
+			'00' + vec3hashing.charset.indexOf(encodedString[0]).toString(4).slice(-3).split('');
+		const minX2 = (deltaX * (maxX - minX)) / 4 + minX;
+		const maxX2 = ((deltaX + 1) * (maxX - minX)) / 4 + minX;
+		const minY2 = (deltaY * (maxY - minY)) / 4 + minY;
+		const maxY2 = ((deltaY + 1) * (maxY - minY)) / 4 + minY;
+		const minZ2 = (deltaZ * (maxZ - minZ)) / 4 + minZ;
+		const maxZ2 = ((deltaZ + 1) * (maxZ - minZ)) / 4 + minZ;
+		if (encodedString.length > 1)
+			return vec3hashing.encode({
+				...params,
+				minX: minX2,
+				maxX: maxX2,
+				minY: minY2,
+				maxY: maxY2,
+				minZ: minZ2,
+				maxZ: maxZ2,
+				encodedString: `${encodedString.substring(1)}`
+			});
+		return {
+			x: (minX2 + maxX2) / 2,
+			y: (minY2 + maxY2) / 2,
+			z: (minZ2 + maxZ2) / 2
+		};
 	}
 };
