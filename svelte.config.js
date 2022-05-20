1183a9f0c28d6cec117d7239475430927bb4192c
wavesforthemasses <wavesforthemasses@gmail.com>
2022-05-20 13:39:05 +0200

    3 files modified

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,22 @@
 import adapter from '@sveltejs/adapter-auto';
+import { Server } from 'socket.io';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter(),
+		plugins: [
+			{
+				name: 'sveltekit-socket-io',
+				configureServer(server) {
+					const io = new Server(server.httpServer);
+
+					// Socket.IO stuff goes here
+
+					console.log('SocketIO injected');
+				}
+			}
+		],
 
 		// Override http methods in the Todo forms
 		methodOverride: {
